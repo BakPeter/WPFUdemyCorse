e2d260fb5a90b87c75694a16a78d1e53600bde9b
BakPeter <bak.pet@gmail.com>
2022-06-18 14:05:28 +0300

    WIP - calculator app

---
+++ Calculator.App/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Calculator.App;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int _counter = 0;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        _counter++;
+        var result = MessageBox.Show($"Count: {_counter}", "Counter Shower", MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+    }
+}
